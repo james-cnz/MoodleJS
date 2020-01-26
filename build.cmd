cc0c3de3b9a5bed2a3f4c291d02e76b44f0533c8
james-cnz <5689414+james-cnz@users.noreply.github.com>
2020-01-27 10:03:30 +1300

    Ignore files, add note

--- build.cmd
+++ build.cmd
@@ -1,4 +1,6 @@
 REM Build script
+REM First go to https://unpkg.com/webextension-polyfill/dist/
+REM and get browser-polyfill.js & browser-polyfill.js.map
 REM TODO: continue on error
 tsc
 tslint --project .
